5c5ced6df870da43d0d2c71e8dedba78bcc46a52
Nicolas Rigaudiere <nicolas.rigaudiere@gmail.com>
2016-02-04 16:20:48 +0100

    Edited testimonial layout + added contact google map

--- assets/js/custom/customJQuery.js
+++ assets/js/custom/customJQuery.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 
 // Carousel
 $(document).ready(function() {
-    $('.carousel').carousel({interval: 7000});
+    $('#homeCarousel').carousel({interval: 7000});
 
 	$('#homeCarousel').bind('slid.bs.carousel', function (e) {
 		var index = $('.carousel-inner .active').index();
@@ -34,6 +34,8 @@ $(document).ready(function() {
 		$('.carousel-controllers').children('[data-slide-to=\"'+index+'\"]').addClass('active');
 
 	});
+
+	$('#testimonialCarousel').carousel({interval: 3000});
   });
 
 
@@ -51,3 +53,68 @@ $('.instagram').instagram({
 	// code:'ab74a752bf5e4171a06f499f80db52de'
 	accessToken: '2896544984.5e17e41.13df3b4f314048a6ad5a001a1dc7f379'
 });
+
+
+// Google Maps
+// function initMap(){
+
+// 	var styleArray = [{
+// 		featureType: "all",
+// 		elementType: "all",
+// 		stylers: [
+// 			{ saturation: -100 } // <-- THIS
+// 		]
+// 	}];
+
+// 	var mapCanvas = document.getElementById('map');
+// 	var mapOptions = {
+// 		center: new google.maps.LatLng(48.871082,2.339652),
+// 		zoom:15,
+// 		scrollwheel:false,
+// 		styles: styleArray,
+// 		mapTypeId:google.maps.MapTypeId.ROADMAP
+// 	}
+
+// 	var map = new google.maps.Map(mapCanvas,mapOptions)
+// 	var myLatLng = new google.maps.LatLng(48.869487,2.339695);
+// 	var marker = new google.maps.Marker(
+// 		{
+// 			position:myLatLng,
+// 			map:map,
+// 			draggable:false,
+// 			icon:'http://www.srpf.fr/wp-content/uploads/2015/10/logo_micro.png',
+// 			title:'S.R.P.F : Société de Revalorisation du Patrimoine Français'
+// 		});
+
+// 	var contentString = '<div id="content">S.R.P.F : Société de Revalorisation du Patrimoine Français</div>';
+// 	var infowindow=new google.maps.InfoWindow({content:contentString});
+// 	infowindow.open(map,marker);
+// }
+// 
+function initMap() {
+
+	var styleArray = [{
+		featureType: "all",
+		elementType: "all",
+		stylers: [{ saturation: -100 }]
+	}];
+	var myLatLng = {lat: 41.385733, lng: 2.181448};
+
+	// Create a map object and specify the DOM element for display.
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: myLatLng,
+		scrollwheel: false,
+		styles: styleArray,
+		zoom: 16
+	});
+
+	var contentString = '<div id="content">BANANA BREAD</div>';
+	
+	var infowindow=new google.maps.InfoWindow({
+			content:contentString,
+			position: myLatLng
+		});
+	
+	infowindow.open(map);
+
+}
