2c27b4b7a07b4896a75e07dfe722d47057a9d71e
Nicolas Rigaudiere <nicolas.rigaudiere@gmail.com>
2016-02-12 11:29:35 +0100

    Reorganized templates

--- assets/js/custom/customJQuery.js
+++ assets/js/custom/customJQuery.js
@@ -28,10 +28,7 @@ $(document).ready(function () {
 	$(".navbar-nav li a").click(function(event) {
 		$(".navbar-collapse").collapse('hide');
 	});
-
-	// $(".navbar-toggle").click(function(event) {
-	// 	$(".navbar-collapse").collapse('hide');
-	// });
+  
 });
 
 
